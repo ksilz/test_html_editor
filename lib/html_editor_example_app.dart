79ae422b9e73d26759644720bd403db0dea934f2
Karsten Silz <karsten.silz@outlook.com>
2022-02-24 20:44:06 +0000

    Throws errors with iOS.

--- lib/html_editor_example_app.dart
+++ lib/html_editor_example_app.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:html_editor_enhanced/html_editor.dart';
+import 'package:touch_indicator/touch_indicator.dart';
 
 class HtmlEditorExample extends StatefulWidget {
   HtmlEditorExample({Key? key, required this.title}) : super(key: key);
@@ -18,38 +20,59 @@ class _HtmlEditorExampleState extends State<HtmlEditorExample> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        elevation: 0,
+    var theChild = HtmlEditor(
+      controller: controller,
+      htmlEditorOptions: HtmlEditorOptions(
+        hint: 'Your text here...',
+        shouldEnsureVisible: true,
+        //initialText: "<p>text content initial, if any</p>",
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            HtmlEditor(
-              controller: controller,
-              htmlEditorOptions: HtmlEditorOptions(
-                hint: 'Your text here...',
-                shouldEnsureVisible: true,
-                //initialText: "<p>text content initial, if any</p>",
-              ),
-              htmlToolbarOptions: HtmlToolbarOptions(
-                defaultToolbarButtons: [
-                  OtherButtons(fullscreen: false, codeview: false, help: false),
-                  FontButtons(superscript: false, subscript: false, strikethrough: false),
-                  ListButtons(listStyles: false, ol: true, ul: true),
-                  InsertButtons(link: true, picture: false, audio: false, video: false, otherFile: false, table: false, hr: false),
+      htmlToolbarOptions: HtmlToolbarOptions(
+        defaultToolbarButtons: [
+          OtherButtons(fullscreen: false, codeview: false, help: false, undo: false, redo: false),
+          FontButtons(superscript: false, subscript: false, strikethrough: false),
+          ListButtons(listStyles: false, ol: true, ul: true),
+          InsertButtons(link: true, picture: false, audio: false, video: false, otherFile: false, table: false, hr: false),
+        ],
+        toolbarPosition: ToolbarPosition.belowEditor,
+        toolbarType: ToolbarType.nativeGrid,
+        renderSeparatorWidget: false,
+      ),
+      otherOptions: OtherOptions(),
+    );
+
+    return PlatformScaffold(
+      material: (_, __) => MaterialScaffoldData(
+        extendBodyBehindAppBar: true,
+        extendBody: true,
+      ),
+      cupertino: (_, __) => CupertinoPageScaffoldData(resizeToAvoidBottomInset: false, resizeToAvoidBottomInsetTab: false),
+      appBar: PlatformAppBar(
+        title: Text(widget.title),
+        material: (_, __) => MaterialAppBarData(
+          automaticallyImplyLeading: true,
+        ),
+        cupertino: (_, __) => CupertinoNavigationBarData(
+          transitionBetweenRoutes: false,
+        ),
+      ),
+      body: Localizations(
+        locale: const Locale('en', 'US'),
+        delegates: <LocalizationsDelegate<dynamic>>[
+          DefaultWidgetsLocalizations.delegate,
+          DefaultMaterialLocalizations.delegate,
+        ],
+        child: TouchIndicator(
+          enabled: false,
+          child: SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  theChild,
                 ],
-                toolbarPosition: ToolbarPosition.belowEditor,
-                //by default
-                toolbarType: ToolbarType.nativeGrid,
-                //by default
-                renderSeparatorWidget: false,
               ),
-              otherOptions: OtherOptions(),
             ),
-          ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_localized_locales/flutter_localized_locales.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 import 'html_editor_example_app.dart';
 
@@ -12,21 +16,39 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: HtmlEditorExample(title: 'Flutter Demo Home Page'),
+    final myBrightTheme = Theme.of(context).copyWith(
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+      brightness: Brightness.light,
+      primaryColor: Colors.black,
+      textTheme: Theme.of(context).textTheme.copyWith(bodyText1: Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: Platform.isIOS ? 17 : 14)),
     );
+    final myDarkTheme = Theme.of(context).copyWith(
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+      brightness: Brightness.dark,
+      primaryColor: Colors.white,
+      textTheme: Theme.of(context).textTheme.copyWith(bodyText1: Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: Platform.isIOS ? 17 : 14)),
+    );
+
+    final app = PlatformApp(
+      title: 'HTML Editor on iOS',
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: [LocaleNamesLocalizationsDelegate()],
+      material: (_, __) => MaterialAppData(
+        theme: myBrightTheme,
+        darkTheme: myDarkTheme,
+        themeMode: ThemeMode.system,
+      ),
+      home: HtmlEditorExample(title: 'HTML Editor on iOS'),
+    );
+
+    return Platform.isIOS
+        ? Localizations(
+            locale: const Locale('en', 'US'),
+            delegates: <LocalizationsDelegate<dynamic>>[
+              DefaultWidgetsLocalizations.delegate,
+              DefaultMaterialLocalizations.delegate,
+            ],
+            child: app)
+        : app;
   }
 }
